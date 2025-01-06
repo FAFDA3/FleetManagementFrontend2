6a487fe9c1f5f032f96b7e42f7edb60c4e415526
Marcfio <marcofioravanti.uts@gmail.com>
2025-01-06 11:25:57 +0100

    ready to deploy

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
-   // base_url: "https://mfflettmanagementbackend-6551d4a16fc3.herokuapp.com/api", 
+    base_url: "https://mfflettmanagementbackend-6551d4a16fc3.herokuapp.com/api", 
 
-    base_url: "http://localhost:3001/api", 
+   // base_url: "http://localhost:3001/api", 
   };
   
   export default config;
